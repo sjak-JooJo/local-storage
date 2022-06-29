fad81c3d59afa7fa0c430cbae4465896be7ecefd
JooJo <sjak0101@gmail.com>
2022-06-29 04:14:06 +0100

    first commit

---
+++ alert.js
@@ -0,0 +1,24 @@
+console.log('alert i am coming yoooo');
+//alert('ma is coming open the book');
+
+const maComing = () =>{
+    alert('ammu is coming. Open the book');
+}
+const askPicnic = () =>{
+   const response = confirm('Are you going to picnic');
+   console.log(response);
+   if(response === true){
+    alert('amake fee ta bkash kore de');
+   }
+   else{
+    console.log('DGM !!! doore giya mor');
+   }
+}
+
+const askName = () =>{
+   const name = prompt('What is your name?');
+   if(name){
+        console.log(name);
+   }
+   
+}
---
+++ explore.js
@@ -0,0 +1,12 @@
+const name = 15;
+const student = {
+    name: 'joly', 
+    friend: 'borsha', 
+    job:'Business', 
+    makeMovie: function(name){
+    console.log(name);
+    }
+};
+student.makeMovie('din rat');
+//document.getElementById('name');
+alert('abc');
